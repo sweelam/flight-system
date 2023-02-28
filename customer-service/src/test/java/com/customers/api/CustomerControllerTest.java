ab91c54bf5790e42f50b5da76eb3b629c03a9a88
msweelam <msweelam@unifonic.com>
2023-02-28 21:36:04 +0400

    remove unused imports

--- customer-service/src/test/java/com/customers/api/CustomerControllerTest.java
+++ customer-service/src/test/java/com/customers/api/CustomerControllerTest.java
@@ -1,6 +1,5 @@
 package com.customers.api;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
