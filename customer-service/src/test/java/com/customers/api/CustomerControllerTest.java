f15185f5370b8a9a4462b52671531debbef66e85
msweelam <msweelam@unifonic.com>
2023-02-28 21:35:46 +0400

    Add for customerController

---
+++ customer-service/src/test/java/com/customers/api/CustomerControllerTest.java
@@ -0,0 +1,39 @@
+package com.customers.api;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class CustomerControllerTest {
+    private final String URL = "/api/v1/customers";
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testGetAllCustomersShouldReturnOk() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URL)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetCustomerByEmailShouldReturnOk() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URL + "/md.ahmed@gmail.com")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+}
