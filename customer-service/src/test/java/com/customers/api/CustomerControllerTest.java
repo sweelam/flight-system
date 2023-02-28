61d7fcdbe03c0a827a63b1f8de5a5e56bd0b6db8
msweelam <msweelam@unifonic.com>
2023-02-28 23:08:57 +0400

    disable jaeegar in testing

--- customer-service/src/test/java/com/customers/api/CustomerControllerTest.java
+++ customer-service/src/test/java/com/customers/api/CustomerControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CustomerControllerTest {
     private final String URL = "/api/v1/customers";
     @Autowired
